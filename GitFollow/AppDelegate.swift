d1052018b9209bd95634aae1149d411590b1b224
Szymon Michalak <sz.p.michalak@gmail.com>
2024-10-04 09:22:47 +0200

    Initial Commit

---
+++ GitFollow/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  GitFollow
+//
+//  Created by Szymon Michalak on 04/10/2024.
+//
+//  Description:
+//  This file is part of the GitFollow project. It is designed to 
+//  [briefly describe what this file/class/module does].
+//
+//  Personal Notes:
+//  [Add any personal notes, thoughts, or reminders here. For example, why
+//  you chose a particular design pattern, or any insights for future you.]
+//
+//  Find me here:
+//  iOSwift.dev: https://ioswift.dev/
+//
+
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ GitFollow/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GitFollow
+//
+//  Created by Szymon Michalak on 04/10/2024.
+//
+//  Description:
+//  This file is part of the GitFollow project. It is designed to 
+//  [briefly describe what this file/class/module does].
+//
+//  Personal Notes:
+//  [Add any personal notes, thoughts, or reminders here. For example, why
+//  you chose a particular design pattern, or any insights for future you.]
+//
+//  Find me here:
+//  iOSwift.dev: https://ioswift.dev/
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
